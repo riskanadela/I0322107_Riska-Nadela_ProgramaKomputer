db52c337fb353044396768bad990cf78ef3379a1
dela <riskanadela09@student.uns.ac.id>
2023-03-12 22:15:44 +0700

    excercise-01

---
+++ Modul_01/Excecise-01.py
@@ -0,0 +1,16 @@
+Nama     = "Riska Nadela"
+NIM      = "I0322107"
+Tempat_Tinggal  = "Tangerang"
+print("===============================================================================================================================================================================")
+print("Nama   =" , Nama)
+print("NIM    =", NIM)
+print("===============================================================================================================================================================================")
+print(Tempat_Tinggal)
+print('''
+Aku tinggal di Kota Tangerang. Tangerang merupakan kota yang terletak di Provinsi Banten, karena merupakan salah satu kota yang berada dekat Ibu Kota maka tentunya secara pemandangan Kota Tangerang sudah sangat modern dan dipenuhi oleh Gedung-gedung tinggi dam pemukiman masyarakat. Meskipun sama seperti kota besar lainnya yang terisi Gedung-gedung,namun daerah tempat tinggalku tetap tidak melupakan pentingnya tanaman, terbukti dengan masih banyak ditemuinya tanaman atau pohon disekitar bangunan-bangunan yang ada. Bisa dikatakan kota Tangerang merupakan kota dengan industry pabrik dan manufaktur terbanyak di Indonesia. Salah satu tempat yang tidak asing yang berada di Tangerang adalah adanya Bandara International Soekarno-Hatta.
+''')
+print('''
+Harapan setelah menjadi lulusan Teknik Industri bagi saya adalah tentunya langsung mendapatkan kerja, tidak perlu langsung berpangkat tinggi ataupun bergaji besar yang penting adalah untuk mencari pengalaman lebih. Pastinya juga berharap mampu bekerja dibidang yang menjadi passion saya dan membuat saya nyaman dan bangga sebagai lulusan Teknik Industri.
+''')
+
+print("===============================================================================================================================================================================")
---
+++ Modul_01/dela.py
@@ -0,0 +1,13 @@
+nama depan = "Riska"
+nama blk = "Nadela"
+
+angka = 10
+desimal - 0.5
+boolean = True
+kelas_D = ["riska", "kls d", "dll"]
+data = {"nama": "riska", "kelas": "KLS D", "nim": 107}
+
+print(nama_depan)
+print("nama saya adalah riska")
+
+print(f"nama saya adalah {nama_depan} {nama_blk} dari kelas {data['kelas']}")
